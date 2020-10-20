77537a86946d6c2a52edac74045bdc90eb636750
Charlie Getzen <charliegetzenlc@gmail.com>
2020-10-20 18:58:24 -0500

    extra arg

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 variable "bind_data" {}
 
+variable "extra_arg" {}
+
 output "client_data" {
-  value = {"internal": var.bind_data["ip_address"], "external": "123.222.333.444"}
+  value = {
+    "internal": var.bind_data["ip_address"], 
+    "external": "123.222.333.444",
+    "credentials": {
+      "instructions": "Download networking terraform here"
+    },
+  }
 }
